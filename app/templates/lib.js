93514b44dd187bfffb5e1787a63d89acf3f33076
Sebastian Schürmann <sebs@2xs.org>
2016-10-02 20:30:35 +0200

    read stdin or args

--- app/templates/lib.js
+++ app/templates/lib.js
@@ -4,7 +4,29 @@ module.exports = function () {
     parse: function(program) {
       program = program;
       var p = new Promise(function(resolve, reject) {
-        resolve(program)
+        if (program.args.length < 1) {
+          var stdInData;
+          process.stdin.setEncoding('utf8');
+
+          process.stdin.on('readable', () => {
+            var chunk = process.stdin.read();
+            if (chunk !== null) {
+              program.args[0] = chunk;
+              resolve(program)
+            } else if (!program.args[0]) {
+              program.help();
+              process.exit(1);
+            }
+          });
+          process.stdin.on('end', () => {
+            if (!program.args[0]) {
+              reject('no arguments');
+            }
+          });
+
+        } else {
+          resolve(program)
+        }
       });
       return p;
     }
